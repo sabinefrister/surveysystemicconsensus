149d3b9db605b2e3d0f0639b0466d94117a183b0
sabinefrister <sabine@frister.net>
2020-12-13 19:19:01 +0100

    Add Formik and create first form to test behavior.

--- src/CreateSurvey.js
+++ src/CreateSurvey.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
+import { Formik, Field, Form } from 'formik';
 
 
 class CreateSurvey extends Component {
 	render() {
     return (
-			<div>
+			<React.Fragment>
 				<h2>Create your Survey</h2>
-			</div>
+				<Formik
+		      initialValues={{
+		        option1: '',
+		        option2: '',
+		      }}
+		      onSubmit={(values) => {
+		        console.log(values)
+		      }}
+		    >
+					<Form>
+						<label htmlFor="option1">Option 1</label>
+        		<Field id="option1" name="option1" placeholder="option" />
+        		<label htmlFor="option2">Option 2</label>
+        		<Field id="option2" name="option2" placeholder="option" />
+		        <button type="submit">Submit</button>
+					</Form>
+				</Formik>
+			</React.Fragment>
     );
   }
 }
