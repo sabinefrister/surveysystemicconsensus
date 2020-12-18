b09a8773a0bd7bc00c7e44706f2dc2537303c546
sabinefrister <sabine@frister.net>
2020-12-18 19:19:28 +0100

    Fix problem with missing initial values and form inside a table.

--- src/Survey.js
+++ src/Survey.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
-import { Header, Table } from 'semantic-ui-react'
+import { Header, Table, Button } from 'semantic-ui-react'
 import { Formik, Field, Form, ErrorMessage, FieldArray } from 'formik';
-
-
 
 
 class Survey extends Component {
@@ -22,53 +20,66 @@ class Survey extends Component {
 				<Header as='h1'>Join this Survey</Header>
 				<p>Here is your link for this survey: <a>https://www.yoursurvey.com</a></p>
 				<Header as='h2'>{this.props.surveyData.surveyTitle}</Header>
+    		<Formik
+    			initialValues={{
+    				attendeeName: '',
+            option1: '',
+            option2: '',
+          }}
+		      onSubmit={(surveyData) => {
+		        console.log(surveyData)
+		      }}
+		    >
+		    	<Form>
+						<Table basic='very' celled collapsing>
+					    <Table.Header>
+					      <Table.Row>
+					      	<Table.HeaderCell>Name</Table.HeaderCell>
+					      	{this.props.surveyData.options.map(option => (
+								    <Table.HeaderCell>abc{option.option}</Table.HeaderCell>
+									))}
+					      </Table.Row>
+					    </Table.Header>
+					    <Table.Body>
+					    	{/* user already taken the survey */}
+					    	{dataFromServer.map((attendee, index) => (
+						    	<Table.Row key={`${attendee.name}_${index}`}>		
+						        <Table.Cell key={`${attendee.name}_${index}_cell1`}>{attendee.name}</Table.Cell>
+						        <Table.Cell key={`${attendee.name}_${index}_cell2`}>{attendee.option1}</Table.Cell>
+						        <Table.Cell key={`${attendee.name}_${index}_cell3`}>{attendee.option2}</Table.Cell>
+						      </Table.Row>
+					    	))}
 
-				<Table basic='very' celled collapsing>
-			    <Table.Header>
-			      <Table.Row>
-			      	<Table.HeaderCell>Name</Table.HeaderCell>
-			      	{this.props.surveyData.options.map(option => (
-						    <Table.HeaderCell>abc{option.option}</Table.HeaderCell>
-							))}
-			      </Table.Row>
-			    </Table.Header>
-			    <Table.Body>
-			    	{/* user already taken the survey */}
-			    	{dataFromServer.map((attendee, index) => (
-				    	<Table.Row key={`${attendee.name}_${index}`}>		
-				        <Table.Cell key={`${attendee.name}_${index}_cell1`}>{attendee.name}</Table.Cell>
-				        <Table.Cell key={`${attendee.name}_${index}_cell2`}>{attendee.option1}</Table.Cell>
-				        <Table.Cell key={`${attendee.name}_${index}_cell3`}>{attendee.option2}</Table.Cell>
-				      </Table.Row>
-			    	))}
-			    </Table.Body>
-		    </Table>
-		    <Formik
-				      onSubmit={(surveyData) => {
-				        console.log(surveyData)
-				      }}
-				    >
-				    	<Form>
+					    	<Table.Row>		
+					        <Table.Cell>
 					        	<Field 
 		                  id="attendeeName" 
 		                  name="attendeeName" 
 		                  placeholder="name"
 		                />
-
+					        </Table.Cell>	
+					        <Table.Cell>
 					        	<Field 
 		                  id="option1" 
 		                  name="option1" 
 		                  placeholder="1 ... 10"
 		                />
-
+					        </Table.Cell>	
+					        <Table.Cell>
 					        	<Field 
 		                  id="option2" 
 		                  name="option2" 
 		                  placeholder="1 ... 10"
 		                />
-
-			      	</Form>
-		      	</Formik>
+					        </Table.Cell>
+					        <Table.Cell>
+					        	<Button type="submit">Submit your choices</Button>
+					        </Table.Cell>
+					      </Table.Row>
+					    </Table.Body>
+				    </Table>
+		    	</Form>
+      	</Formik>
 			</React.Fragment>
     );
   }
