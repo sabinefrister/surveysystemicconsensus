a73a38a2b5e1dae606aa344c546e3a85bfc8322e
sabinefrister <sabine@frister.net>
2021-01-29 14:19:51 +0100

    Connect frontend with backend through heroku api url.

--- react_app/src/apiCall.js
+++ react_app/src/apiCall.js
@@ -1,30 +1,35 @@
+const herokuURL = "https://ccs-node-api.herokuapp.com";
+
+let url = "";
+process.env.PRODUCTION ? url = herokuURL : url = "";
+
 async function getParticipantData() {
-    const response = await fetch('/api/participant-data');
+    const response = await fetch(url + '/api/participant-data');
     return await response.json();
-}
+};
 
 async function getSurveyDataFromServer() {
-    const response = await fetch('/api/survey-data');
+    const response = await fetch(url + '/api/survey-data');
     return await response.json();
-}
+};
 
 async function createSurvey(data) {
-  const response = await fetch(`/api/survey-data`, {
+  const response = await fetch(url + '/api/survey-data', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({surveyData: data})
     })
   return await response.json();
-}
+};
 
 async function createParticipantData(data, id) {
-  const response = await fetch(`/api/participant-data`, {
+  const response = await fetch(url + '/api/participant-data', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({participantData: data, surveyId: id})
     })
   return await response.json();
-}
+};
 
 const api = {getParticipantData, getSurveyDataFromServer, createSurvey, createParticipantData}
-export default api
+export default api;
